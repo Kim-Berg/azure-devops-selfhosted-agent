013e9040188555f7e13347d47e2e3be34f22537f
Ben Kooijman <100195282+benkoben@users.noreply.github.com>
2022-05-11 16:45:11 +0200

    quick commit

--- templater.py
+++ templater.py
@@ -19,7 +19,7 @@ def flatten(d, parent_key='', sep='_'):
             items.append((new_key, v))
     return dict(items)
 
-class Template(object):
+class TemplateWrapper(object):
     def __init__(self, cac_path:str, iac_path:str, templates_folder:str, dsc:dict):
         self.cac_path = cac_path
         self.iac_path =iac_path
@@ -32,7 +32,7 @@ class Template(object):
             loader=FileSystemLoader([self.hcl_template_path, self.playbook_template_path])
         )
 
-    def azuredevops(self, dst_path):
+    def azuredevops(self):
         # prepare runtime for ado template rendering
         self.hcl_template_path = os.path.join(self.template_folder, 'AzureDevOps')
         self.set_env()
@@ -41,8 +41,8 @@ class Template(object):
         green_hcl_template = self.env.get_template('green-image.hcl.j2')
         blue_hcl_template = self.env.get_template('blue-image.hcl.j2')
         # set destination path
-        blue_image_path = os.path.join(dst_path, 'blue-image.pkr.hcl')
-        green_image_path = os.path.join(dst_path, 'green-image.pkr.hcl')
+        blue_image_path = os.path.join(self.iac_path, 'blue-image.pkr.hcl')
+        green_image_path = os.path.join(self.iac_path, 'green-image.pkr.hcl')
         
         with open(green_image_path, 'w') as green_img:
             green_img.write(green_hcl_template.render(dsc=self.dsc))
@@ -50,9 +50,10 @@ class Template(object):
         with open(blue_image_path, 'w') as blue_img:
             blue_img.write(blue_hcl_template.render(dsc=self.dsc))
 
-    def playbook(self, dst_path):
+    def playbook(self):
         playbook_yaml_template = self.env.get_template('playbook.yaml.j2')
-        with open(dst_path, 'w') as playbook:
+        playbook_path = os.path.join(self.cac_path, 'playbook.yaml')
+        with open(playbook_path, 'w') as playbook:
             playbook.write(playbook_yaml_template.render(dsc=self.dsc))
 
 # change path to directory of this script
@@ -65,25 +66,19 @@ DESIRED_STATE_PATH=os.path.abspath('desired_settings.yaml')
 
 with open(DESIRED_STATE_PATH, 'r') as stream:
     dsc = flatten(load(stream.read(), Loader=Loader))
-    agent_type = dsc["deployment_agent_type"]
+    agent_type = dsc["deployment_agent_type"].lower()
 
     cac_path=os.path.abspath(os.path.join('.', 'build_config'))
     iac_path=os.path.abspath(os.path.join('.', 'builds'))
 
-    build = Template(
+    build = TemplateWrapper(
         cac_path=cac_path,
         iac_path=iac_path,
         templates_folder=os.path.abspath('templates'),
         dsc=dsc
     )
 
-    match agent_type:
-        case "AzureDevOps":
-            ado_instance = getattr(build, agent_type.lower())
-            ado_instance(
-                dst_path=iac_path
-            )
-        case "Github":
-            # just to show to scale out code
-            pass
+    create_agent_image_iac = getattr(build, agent_type)
+    create_agent_image_iac()
+    build.playbook()
     
